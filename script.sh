1374cde02a534ffd310e3731c51d15a22ba4dd22
pon98125 <pon98125@naver.com>
2023-02-22 17:49:43 +0900

    commit

---
+++ script.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+alias kpod='kubectl get pod -A -o wide'
+alias knode='kubectl get node -o wide'
+alias kdep='kubectl get deployment -A'
+alias kall='kubectl get all -A'
+alias khpa='kubectl get hpa -A'
+alias kdes='kubectl describe'
+alias kget='kubectl get'
